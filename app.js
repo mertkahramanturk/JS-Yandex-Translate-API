acd4a0b6e4ee3e8f5f627380126cfb9d75bcdbef
Mert Kahramantürk <kahramanturkk@gmail.com>
2022-09-08 10:57:33 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Prototype, Ajax, Callback
+
+eventListeners();
+
+function eventListeners(){
+
+
+    document.getElementById("translate-form").addEventListener("submit", translateWord);
+    //Change
+    document.getElementById("language").onchange = function(){
+        //Arayüz İşlemleri
+    }
+
+}
+
+function translateWord(e){
+
+
+
+
+
+    e.preventDefault();
+
+
+
+}
---
+++ translate.js
@@ -0,0 +1,15 @@
+function Translate(word, language){
+
+    this.apikey = "#";
+    this.word = word;
+    this.language = language;
+
+    //XHR Object
+
+    this.xhr = XMLHttpRequest()
+}
+
+Translate.prototype.translateWord = function(callback){
+    //AJAX İşlemleri
+    const endpoint = `#`;
+};
